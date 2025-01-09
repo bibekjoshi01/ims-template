76ace882129a6284f645a6662b973a26cfd7149e
Aman Khanal <amankhanal1960@gmail.com>
2025-01-09 19:09:11 +0545

    logo transparent

--- src/components/logo/LogoMain.jsx
+++ src/components/logo/LogoMain.jsx
@@ -1,5 +1,5 @@
 // project import
-import SiteLogo from 'assets/images/logo.png';
+import SiteLogo from 'assets/images/logo2.png';
 
 // ==============================|| LOGO SVG ||============================== //
 
