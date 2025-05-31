7236e475554612b5e2d490a9de66a4d1d6eb5967
bibekjoshi01 <bibekjoshi34@gmail.com>
2025-05-31 13:03:44 +0545

    Fix: logo and title changed

--- src/components/logo/LogoMain.jsx
+++ src/components/logo/LogoMain.jsx
@@ -4,7 +4,7 @@ import SiteLogo from '@/assets/images/logo.png';
 // ==============================|| LOGO SVG ||============================== //
 
 const Logo = () => {
-  return <img src={SiteLogo} alt="Logo Main" width="100" />;
+  return <img src={SiteLogo} alt="Logo Main" width="80" />;
 };
 
 export default Logo;
